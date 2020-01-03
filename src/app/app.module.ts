8ba1668bb709981a6ce179fa3a181a8162ce16e5
kiranlasure <kiranlasure95@gmail.com>
2020-01-03 07:12:32 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ,FlexLayoutModule ],
-  declarations: [ AppComponent],
+  declarations: [ AppComponent, LoginComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
