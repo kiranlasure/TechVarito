8ea99555b2e1617de1eab098a74fc8532693a482
kiranlasure <kiranlasure95@gmail.com>
2020-01-03 14:43:10 +0530

    3 files modifiedNothing

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import { FormGroup, FormControl, Validators, FormBuilder }  from '@angular/forms';
+// import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,7 +11,26 @@ export class RegisterComponent implements OnInit {
 
   constructor() { }
 
+//   userForm = new FormGroup({
+// 	name: new FormControl('Mahesh', Validators.maxLength(10)),
+// 	age: new FormControl(20, Validators.required),
+// 	city: new FormControl(),
+// 	country: new FormControl()
+//  });
   ngOnInit() {
   }
 
+  name2 = " ";
+  signup(val)
+  {
+      this.name2=val;
+      localStorage.setItem("username",this.name2);
+      // console.log(val);
+  }
+
+  getItem()
+  {
+    console.log(localStorage.getItem("username"));
+  }
+  
 }
