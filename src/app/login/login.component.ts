904bb94bec91ddae1a22434a631f670a71e967a5
kiranlasure <kiranlasure95@gmail.com>
2020-01-03 09:33:13 +0530

    first routing

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+
+  gotoReg()
+  {
+        this.router.navigate['/reg']
+  }
+
+  cl()
+  {
+    console.log("hi");
   }
 
 }
